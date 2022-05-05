cd4e0da899af2349cf5325c17af3b4ccd0e18a1b
anushakuppahally <aak154@georgetown.edu>
2022-05-05 15:55:46 -0400

    Update test_spotify_service.py
    Using fixture

--- tests/test_spotify_service.py
+++ tests/test_spotify_service.py
@@ -2,24 +2,25 @@ import os
 import pytest 
 from dotenv import load_dotenv
 
-from app.spotify_service import SpotifyService
-service = SpotifyService()
-
-
 load_dotenv()
+
+from app.spotify_service import SpotifyService
+@pytest.fixture(scope="module")
+def spotify_service():
+    return SpotifyService()
 
 @pytest.mark.skipif(os.getenv("CI")=="true", reason="avoid issuing on the CI server") # skips this test on CI
 def test_get_artist():
-    assert service.get_artist("Taylor Swift") == "spotify:artist:06HL4z0CvFAxyc27GXpf02"
+    assert spotify_service.get_artist("Taylor Swift") == "spotify:artist:06HL4z0CvFAxyc27GXpf02"
 
 @pytest.mark.skipif(os.getenv("CI")=="true", reason="avoid issuing on the CI server") # skips this test on CI
 def test_verifyartist():
-    artist = service.verify_artist("spotify:artist:06HL4z0CvFAxyc27GXpf02") #artist will look like "Taylor Swift"
+    artist = spotify_service.verify_artist("spotify:artist:06HL4z0CvFAxyc27GXpf02") #artist will look like "Taylor Swift"
     assert isinstance(artist,str)
 
 @pytest.mark.skipif(os.getenv("CI")=="true", reason="avoid issuing on the CI server") # skips this test on CI
 def test_artist_albums():
-    results = service.artist_albums("spotify:artist:06HL4z0CvFAxyc27GXpf02")
+    results = spotify_service.artist_albums("spotify:artist:06HL4z0CvFAxyc27GXpf02")
     assert isinstance(results,list)
     album = results[0] #album will look like this: ('1989', 'Released:', '2014-10-27', 'Total Tracks:', 13)
     assert len(album) == 5
@@ -27,7 +28,7 @@ def test_artist_albums():
 
 @pytest.mark.skipif(os.getenv("CI")=="true", reason="avoid issuing on the CI server") # skips this test on CI
 def test_artist_top_tracks():
-    results = service.artist_top_tracks("spotify:artist:06HL4z0CvFAxyc27GXpf02")
+    results = spotify_service.artist_top_tracks("spotify:artist:06HL4z0CvFAxyc27GXpf02")
     assert isinstance(results,list)
     assert len(results) == 5
     track = results[0] #track will look like 'Ribs'
@@ -35,7 +36,7 @@ def test_artist_top_tracks():
 
 @pytest.mark.skipif(os.getenv("CI")=="true", reason="avoid issuing on the CI server") # skips this test on CI
 def test_artist_song_recs():
-    results = service.artist_song_recommendations("spotify:artist:06HL4z0CvFAxyc27GXpf02")
+    results = spotify_service.artist_song_recommendations("spotify:artist:06HL4z0CvFAxyc27GXpf02")
     assert isinstance(results,list)
     songrec = results[0] #songrec will look like ('Adore You', 'by', 'Harry Styles')
     assert len(songrec) == 3
@@ -43,7 +44,7 @@ def test_artist_song_recs():
 
 @pytest.mark.skipif(os.getenv("CI")=="true", reason="avoid issuing on the CI server") # skips this test on CI
 def test_artist_recs():
-    results = service.artist_recs("spotify:artist:06HL4z0CvFAxyc27GXpf02")
+    results = spotify_service.artist_recs("spotify:artist:06HL4z0CvFAxyc27GXpf02")
     assert isinstance(results,list)
     artistrec = results[0] #artistrec will look like 'Taylor Swift'
     assert isinstance(artistrec,str)
@@ -51,7 +52,7 @@ def test_artist_recs():
 @pytest.mark.skipif(os.getenv("CI")=="true", reason="avoid issuing on the CI server") # skips this test on CI
 def test_get_characteristics():
     id = "spotify:track:51QEyJI5M7uyd8DOh9tqQY" #Buzzcut Season by Lorde
-    results = service.get_characteristics(id)
+    results = spotify_service.get_characteristics(id)
     assert len(results) == 15
     assert isinstance(results, list)
     assert isinstance(results[4],int) #length
