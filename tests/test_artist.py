23b8db5ec1a88af589f1cd3c2b893b6ee47ad4f0
anushakuppahally <aak154@georgetown.edu>
2022-05-05 12:40:42 -0400

    Update test_artist.py

--- tests/test_artist.py
+++ tests/test_artist.py
@@ -6,30 +6,10 @@ from app.artist_analysis import ArtistRecs
 
 import os
 import pytest 
-import spotipy
-import requests
 from dotenv import load_dotenv
-from spotipy.oauth2 import SpotifyClientCredentials
+
 
 load_dotenv()
-
-CLIENT_ID = os.getenv('SPOTIPY_CLIENT_ID')
-CLIENT_SECRET = os.getenv('SPOTIPY_CLIENT_SECRET')
-spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
-BASE_URL = 'https://api.spotify.com/v1/'
-AUTH_URL = 'https://accounts.spotify.com/api/token'
-auth_response = requests.post(AUTH_URL, {
-        'grant_type': 'client_credentials',
-        'client_id': CLIENT_ID,
-        'client_secret': CLIENT_SECRET,
-    })
-
-#convert the response to json
-auth_response_data = auth_response.json()
-
-#save the access token
-access_token = auth_response_data['access_token']
-headers = {'Authorization': 'Bearer {token}'.format(token=access_token)}
 
 @pytest.mark.skipif(os.getenv("CI")=="true", reason="avoid issuing on the CI server") # skips this test on CI
 def test_getartist():
